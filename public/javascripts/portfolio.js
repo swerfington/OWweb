8d9126feb6e617c0fb2f6a0a3e5a109f9014629d
sworth <sworth08@gmail.com>
2018-08-28 09:23:03 +0100

    about changes

--- public/javascripts/portfolio.js
+++ public/javascripts/portfolio.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    var images = ['subtle_grunge.png', 'subtle_white_feathers.png', 'egg_shell.png', 'ep_naturalwhite.png', 'extra_clean_paper.png', 'natural_paper.png', 'wall4.png'];
+    var images = ['subtle_grunge.png', 'subtle_white_feathers.png', 'ep_naturalwhite.png', 'extra_clean_paper.png', 'natural_paper.png', 'wall4.png'];
     var randomNumber = Math.floor(Math.random() * images.length);
     var bgImg = 'url(images/backgrounds/' + images[randomNumber] + ')';
 
